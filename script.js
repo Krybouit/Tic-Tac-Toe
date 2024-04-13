22d0ed4fe9a1b7bba67254795196ac891ad2587b
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-04-13 19:55:03 -0400

    Remove for loop and add new function
    *removes the for loop that joined the board array.
    
    *adds a work in progress function that will store
    the players and control the flow of game.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const GameBoard = {
-    board: [[1, 2, 3], 
-            [4, 5, 6], 
-            [7, 8, 9]]
+const gameBoard = {
+     board: [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]],       
 };
 
-const player1 = {
-    name: "Blue"
-}
 
-const player2 = {
-    name: "Red"
-}
+function gameController() {
+    const players = [
+        {
+            name: "Blue",
+            symbol: "O"
+        },
+        {
+            name: "Red",
+            symbol: "X"
+        }
+    ]
+   
+};
 
 
-for(let row of GameBoard.board) {
-    const rowString = row.join(" ");
-    console.log(rowString);
-}
+console.log(gameBoard.board)
